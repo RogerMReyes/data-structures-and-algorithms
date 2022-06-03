cac315db91ede1c9314a5bf25618418bef98d07d
RogerMReyes <Rogerrmr14@gmail.com>
2022-06-03 16:06:57 -0700

    Fixed naming

--- java/datastructures/lib/src/main/java/datastructures/trees/BinaryTree.java
+++ java/datastructures/lib/src/main/java/datastructures/trees/BinaryTree.java
@@ -6,42 +6,43 @@ import java.util.List;
 
 public class BinaryTree<T>{
   private TreeNode<T> root;
-  private List<T> list = new ArrayList<>();
+  private final List<T> list = new ArrayList<>();
 
   public BinaryTree() {
   }
 
-  public List<T> preOrder(TreeNode<T> root){
-    list.add(root.getValue());
-    if(root.getLeft() != null){
-      preOrder(root.getLeft());
+  public List<T> preOrder(TreeNode<T> node){
+    list.add(node.getValue());
+    if(node.getLeft() != null){
+      preOrder(node.getLeft());
     }
-    if(root.getRight() != null){
-      preOrder(root.getRight());
-    }
-    return list;
-  }
-
-  public List<T> inOrder(TreeNode<T> root){
-    if(root.getLeft() != null){
-      inOrder(root.getLeft());
-    }
-    list.add(root.getValue());
-    if(root.getRight() != null){
-      inOrder(root.getRight());
+    if(node.getRight() != null){
+      preOrder(node.getRight());
     }
     return list;
   }
 
-  public List<T> postOrder(TreeNode<T> root){
-    if(root != null){
-      if(root.getLeft() != null){
-        postOrder(root.getLeft());
+  public List<T> inOrder(TreeNode<T> node){
+
+    if(node.getLeft() != null){
+      inOrder(node.getLeft());
+    }
+    list.add(node.getValue());
+    if(node.getRight() != null){
+      inOrder(node.getRight());
+    }
+    return list;
+  }
+
+  public List<T> postOrder(TreeNode<T> node){
+    if(node != null){
+      if(node.getLeft() != null){
+        postOrder(node.getLeft());
       }
-      if(root.getRight() != null){
-        postOrder(root.getRight());
+      if(node.getRight() != null){
+        postOrder(node.getRight());
       }
-      list.add(root.getValue());
+      list.add(node.getValue());
     }
     return list;
   }
