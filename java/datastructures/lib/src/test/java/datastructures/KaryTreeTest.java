d5059fc01ac079de021f3611b4ecd7dde05f2712
RogerMReyes <Rogerrmr14@gmail.com>
2022-06-08 15:24:11 -0700

    Completed ReadMe and Code Challenge 18

---
+++ java/datastructures/lib/src/test/java/datastructures/KaryTreeTest.java
@@ -0,0 +1,32 @@
+package datastructures;
+
+import datastructures.kAryTree.KaryNode;
+import datastructures.kAryTree.KaryTree;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class KaryTreeTest {
+
+  @Test
+  void fizzBuzzTest() {
+    KaryTree<KaryNode<Integer>> sut = new KaryTree<>();
+    KaryNode<Integer> sut1 = new KaryNode<>(1);
+    KaryNode<Integer> sut2 = new KaryNode<>(2);
+    KaryNode<Integer> sut3 = new KaryNode<>(3);
+    KaryNode<Integer> sut4 = new KaryNode<>(4);
+    KaryNode<Integer> sut5 = new KaryNode<>(5);
+    sut.setRoot(sut1);
+    sut1.getChild().add(sut2);
+    sut1.getChild().add(sut3);
+    sut2.getChild().add(sut4);
+    sut3.getChild().add(sut5);
+    KaryTree<KaryNode<String>> newSut = sut.fizzBuzzTree(sut);
+    ArrayList<KaryNode<String>> test = newSut.getRoot().getChild();
+    assertEquals("Fizz", test.get(1).getValue());
+  }
+}
