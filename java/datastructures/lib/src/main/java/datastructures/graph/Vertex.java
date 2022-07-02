a13b4b3599fdb65ca65b2a265f9f2400021bcc21
RogerMReyes <Rogerrmr14@gmail.com>
2022-07-01 23:14:28 -0700

    Refactored Hashmap to take edges instead of having it as a property of vertex

--- java/datastructures/lib/src/main/java/datastructures/graph/Vertex.java
+++ java/datastructures/lib/src/main/java/datastructures/graph/Vertex.java
@@ -6,15 +6,11 @@ import java.util.ArrayList;
 
 public class Vertex<T> {
   private T value;
-  ArrayList<Edge<T>> edges = new ArrayList<>();
 
   public Vertex(T value) {
     this.value = value;
   }
 
-  public ArrayList<Edge<T>> getEdges() {
-    return edges;
-  }
 
   public T getValue() {
     return value;
@@ -22,9 +18,5 @@ public class Vertex<T> {
 
   public void setValue(T value) {
     this.value = value;
-  }
-
-  public void setEdges(ArrayList<Edge<T>> edges) {
-    this.edges = edges;
   }
 }
